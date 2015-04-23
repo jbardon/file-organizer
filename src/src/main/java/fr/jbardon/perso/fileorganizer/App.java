83bdb1bdabb50966bd1a8b29ab8c9a3784579f35
jbardon <bardonjeremy1@gmail.com>
2015-04-23 09:50:06 +0200

    Read basic file attributes with java.nio

--- src/src/main/java/fr/jbardon/perso/fileorganizer/App.java
+++ src/src/main/java/fr/jbardon/perso/fileorganizer/App.java
@@ -1,13 +1,33 @@
 package fr.jbardon.perso.fileorganizer;
 
-/**
- * Hello world!
- *
- */
+import static java.lang.System.getProperty;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public static void main(String[] args) throws FileNotFoundException,
+      IOException, org.xml.sax.SAXException {
+ 
+	System.out.println(getProperty("user.dir"));
+    Path file = Paths.get("../sample/organized/2015/03/31-03-2015-1.txt");
+    
+    BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+
+    System.out.println("creationTime: " + attr.creationTime());
+    System.out.println("lastAccessTime: " + attr.lastAccessTime());
+    System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
+
+    System.out.println("isDirectory: " + attr.isDirectory());
+    System.out.println("isOther: " + attr.isOther());
+    System.out.println("isRegularFile: " + attr.isRegularFile());
+    System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
+    System.out.println("size: " + attr.size());
+    
+  }
 }
