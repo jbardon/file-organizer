2bf1fe67cf0a4efa84891bfe3e4ba1666342f0a5
jbardon <bardonjeremy1@gmail.com>
2015-05-09 17:09:02 +0200

    Preserve files attributes during copy

--- src/src/main/java/fr/jbardon/perso/fileorganizer/model/folderaction/FolderActionHardDrive.java
+++ src/src/main/java/fr/jbardon/perso/fileorganizer/model/folderaction/FolderActionHardDrive.java
@@ -3,11 +3,12 @@ package fr.jbardon.perso.fileorganizer.model.folderaction;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.LinkedHashSet;
-import java.util.List;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.util.*;
 
 /**
  * Created by jeremy on 27/04/15.
@@ -48,11 +49,23 @@ public class FolderActionHardDrive implements FolderAction {
         // Copy all files to target
         for(File currentFile : files){
             System.out.println("Copy " + currentFile.getName() + "...");
+
+            Path sourceFile = Paths.get(currentFile.getAbsolutePath()),
+                 targetFile = Paths.get(targetDirectory + "/" + dirName + currentFile.getName());
             try {
                 Files.copy(
-                    Paths.get(currentFile.getAbsolutePath()),
-                        Paths.get(targetDirectory + "/" + dirName + currentFile.getName())
+                    sourceFile,
+                    targetFile,
+                    StandardCopyOption.COPY_ATTRIBUTES
                 );
+
+                // Preserve last modified and access time after to copy
+                BasicFileAttributes attrs = Files.readAttributes(sourceFile, BasicFileAttributes.class);
+
+                Files.setAttribute(targetFile, "creationTime", attrs.creationTime());
+                Files.setAttribute(targetFile, "lastModifiedTime", attrs.lastModifiedTime());
+                Files.setAttribute(targetFile, "lastAccessTime", attrs.lastAccessTime());
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
